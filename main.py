739c9e8d6e635c41196f5597fe57ae425823cb2c
Mallic Tesla <el_mallic@hotmail.com>
2024-05-08 18:54:01 -0300

    acomode unas cosas

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 
-
 from apps.usuario import routers_usuario
 
-api_router = FastAPI()
+app = FastAPI()
 
 # Aca van todas las URLs que dirigen a las apps
-app.include_router (routers_usuario.router, prefix = "/usuario", tags = ["usuario"])
+app.include_router (routers_usuario.router, prefix = "/api", tags = ["api"])
