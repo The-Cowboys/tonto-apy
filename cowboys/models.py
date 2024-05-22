22dc58b3d7b1fa65b0134eea0ffdebc8efc6f629
Mallic Tesla <el_mallic@hotmail.com>
2024-05-22 17:07:13 -0300

    endpoint para agregar un cowboy

---
+++ cowboys/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from settings.bd_coneccion import Base
+
+class Cowboy (Base):
+    # nombre de la tabla a la que apunta este modelo
+    __tablename__ = "cowboys"
+
+    id = Column (Integer, primary_key = True, index = True)
+    name = Column (String, index = True)
+    email = Column (String, unique = True, index = True)
+    tonto = Column (Integer)
+
+    titulos = relationship("Titulo", back_populates="cowboy")
