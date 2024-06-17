b0571150728aa7f120b382efa1ac3e1dbaa3599b
Mallic Tesla <el_mallic@hotmail.com>
2024-06-17 20:49:17 -0300

    preparando el modelo tonto

---
+++ tonto/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, DateTime, ForeignKey, Date
+from sqlalchemy.orm import relationship
+import datetime
+
+from settings.bd_coneccion import Base
+from cowboys.models import Cowboy
+
+class Titulo (Base):
+    __tablename__ = "tonto"
+
+    id = Column (Integer, primary_key = True, index = True)
+    cowboy_id = Column (Integer, ForeignKey ('cowboys.id'))
+    created = Column (DateTime, default = datetime.datetime.utcnow)
+    dia = Column(Date, default=datetime.date.today)
+
+    cowboy = relationship ("Cowboy", back_populates = "tontos")
