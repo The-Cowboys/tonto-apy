d44a50faa3035d3bc285a285405b3cfe2fbc106a
Mallic Tesla <el_mallic@hotmail.com>
2024-06-19 21:40:46 -0300

    solusionando un  error de importacion circular

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+# esto es para evitar errores de importacion circular
+from init_schemas import init_schemas
+init_schemas ()
+
 from usuarios import routers_usuario
 from titulos import routers_titulos
 from cowboys import routers_cowboys
 from titulos import routers_titulos
+# from tonto import routers_tonto
 
 
 app = FastAPI()
@@ -14,3 +19,5 @@ app.include_router (routers_usuario.router, prefix = "/api", tags = ["api"])
 app.include_router (routers_titulos.router, prefix = "/api")
 
 app.include_router (routers_cowboys.router, prefix = "/api")
+
+# app.include_router (routers_tonto.router, prefix = "/api")
