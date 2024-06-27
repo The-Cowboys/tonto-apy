0a7967f83ce85f526a65e7e25b8b0905357c1be2
Mallic Tesla <el_mallic@hotmail.com>
2024-06-26 21:07:18 -0300

    crear tonto despues de sierta hora

--- tonto/crud.py
+++ tonto/crud.py
@@ -1,33 +1,55 @@
 from sqlalchemy.orm import Session
 
 import random
+from datetime import datetime, timedelta, time
 
 from tonto import models, schemas
 from cowboys import crud
 
-
-#   selecsiona un cowboy aleatoreo y le suma mas 1 al tonto y despues lo guarda
-def tonto_random (db: Session):
-    cowboys = crud.obtener_cowboys (db)
-
-    if not cowboys:
-        return None
-
-    random.shuffle (cowboys)
-    random_cowboy = random.choice (cowboys)
-    random_cowboy.tonto += 1
-
-    db.commit()
-
-    return random_cowboy
+# al horario de uruguay se le tiene que sumar 3 hora
+# al horario de europa se le tiene que descontar 2 hora
+HORA_ESPECIFICA = time (21, 0)
 
 
+def obtener_ultimo_tonto (db: Session):
+    ahora = datetime.utcnow()
+    print (ahora)
+    fecha_hora_especifica = datetime.combine (ahora.date(), HORA_ESPECIFICA)
+
+    if ahora < fecha_hora_especifica:
+        fecha_hora_especifica -= timedelta (days=1)
+
+    return db.query (models.Tonto).filter (models.Tonto.created >= fecha_hora_especifica).first()
+
+
+def calcular_tiempo():
+    ahora = datetime.utcnow()
+    fecha_hora_especifica = datetime.combine (ahora.date(), HORA_ESPECIFICA)
+
+    if ahora >= fecha_hora_especifica:
+        fecha_hora_especifica += timedelta (days=1)
+
+    diferencia_tiempo = fecha_hora_especifica - ahora
+    return diferencia_tiempo
+
+
+# veufuca si en las ultimas 24 horas se creo un tonto
 # Crea un nuevo registro en la tabla tonto y le suma mas uno tonto al cowboy
 def nuevo_tonto (db: Session, nuevo_tonto: schemas.NuevoTonto):
+    # Verifica si ya existe un Tonto creado después de la hora específica del día
+    print ("nuevo_tonto", nuevo_tonto)
+    tonto_creado = obtener_ultimo_tonto (db)
+
+    if tonto_creado:
+        print ("1")
+        tiempo_restante = calcular_tiempo()
+        return None, tiempo_restante
+
     cowboy_existente = crud.cowboy_id (db, nuevo_tonto.cowboy_id)
 
     if not cowboy_existente:
-        return None
+        print ("no")
+        return None, None
 
     db_tonto = models.Tonto (cowboy_id = nuevo_tonto.cowboy_id)
 
@@ -37,4 +59,22 @@ def nuevo_tonto (db: Session, nuevo_tonto: schemas.NuevoTonto):
     db.add (db_tonto)
     db.commit()
 
-    return db_tonto
+
+    return db_tonto, None
+
+
+# Selecsiona un cowboy aleatoreo y le suma mas 1 al tonto y despues lo guarda
+# Crea un nuevo registro en la tabla tontos
+def tonto_random (db: Session):
+    cowboys = crud.obtener_cowboys (db)
+
+    if not cowboys:
+        return None
+
+    random.shuffle (cowboys)
+    random_cowboy = random.choice (cowboys)
+    print ("random_cowboy", random_cowboy)
+
+    nuevo_tonto (db, random_cowboy)
+
+    return random_cowboy
--- tonto/routers_tonto.py
+++ tonto/routers_tonto.py
@@ -13,16 +13,23 @@ def seleccionar_tonto (db: Session = Depends (bd_coneccion.get_db)):
     cowboy  = crud.tonto_random (db)
 
     if cowboy is None:
-        raise HTTPException (status_code = 404, detail = "No hay cowboys disponibles")
+        raise HTTPException (status_code = 404, detail = "No existe ese cowboy")
 
     return cowboy
 
 
 @router.post ("/tonto", response_model = schemas.TontoRespuesta)
 def guardar_tonto (nuevo_tonto: schemas.NuevoTonto, db: Session = Depends (bd_coneccion.get_db)):
-    tonto_creado = crud.nuevo_tonto (db, nuevo_tonto)
+    tonto_creado, tiempo_restante = crud.nuevo_tonto (db, nuevo_tonto)
 
     if tonto_creado is None:
-        raise HTTPException (status_code = 404, detail = "No hay cowboys disponibles")
+        if tiempo_restante:
+            horas, minutos = divmod (tiempo_restante.seconds // 60, 60)
+            raise HTTPException(
+                status_code=400,
+                detail=f"El Tonto de hoy ya fue creado. Faltan {horas} horas y {minutos} minutos para que se pueda crear un nuevo Tonto."
+            )
+        else:
+            raise HTTPException(status_code=404, detail="No existe ese cowboy")
 
     return tonto_creado
